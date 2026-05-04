d22c4bbd2e065a8280c7bbaaee92c40db46b33bd
Yaroslav Davkov <work.yaroslav.davkov@gmail.com>
2026-05-04 16:09:15 +0300

    auto formating

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "n", "i", "v" }, "<C-S-s>", function()
+  -- Принудительно загружаем conform если ещё не загружен
+  local conform = require("conform")
+  conform.format({ async = false, lsp_fallback = true }, function()
+    vim.cmd("write")
+    vim.notify("Formatted & saved", vim.log.levels.INFO, { title = "Format" })
+  end)
+end, { desc = "Format & Save", silent = true })
